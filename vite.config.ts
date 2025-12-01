881275dcec1bc04afefd01ec8b8894b43fc3db49
YOUR USER NAME <narenvaithiyaa@gmail.com>
2025-12-01 21:12:55 +0530

    fix for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,38 @@
 import path from "path"
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'RevLine',
+        short_name: 'RevLine',
+        description: 'RevLine Fitness Community Website',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/revline_logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/revline_logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff'
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
